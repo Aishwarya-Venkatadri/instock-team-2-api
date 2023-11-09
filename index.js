81270659de0d14b82733387a7ef8c149f74d5544
Orkhan Ahmadli <ahmedli.orhan@gmail.com>
2023-11-08 22:28:03 -0800

    Swagger setup

--- index.js
+++ index.js
@@ -5,6 +5,38 @@ import inventoryRoutes from "./routes/inventory-routes.js";
 import cors from "cors";
 import bodyParser from "body-parser";
 import rateLimitMiddleware from "./rate-limiter.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+
+
+// Swageer Setup
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const specs = swaggerJSDoc(options);
 
 dotenv.config();
 
@@ -23,6 +55,12 @@ app.use("/warehouses", warehouseRoutes);
 
 // all inventory routes
 app.use("/inventories", inventoryRoutes);
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`running at http://localhost:${PORT}`);
--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -1,3 +1,163 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     inventories:
+ *       type: object
+ *       required:
+ *         - warehouse_id
+ *         - item_name
+ *         - description
+ *         - category
+ *         - status
+ *         - quantity
+ *         - createdAt
+ *         - updatedAt
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the warehouse
+ *         warehouse_id:
+ *           type: string
+ *           description: The name of warehouse
+ *         item_name:
+ *           type: string
+ *           description: The warehouse address
+ *         description:
+ *           type: string
+ *           description: The warehouse city
+ *         category:
+ *           type: string
+ *           description: The warehouse country
+ *         status:
+ *           type: string
+ *           description: The contact name
+ *         quantity:
+ *           type: string
+ *           description: The contact position
+ *         createdAt:
+ *           type: string
+ *           format: date
+ *           description: The date the warehouse was added
+ *         updatedAt:
+ *           type: string
+ *           format: date
+ *           description: The date the warehouse was updated
+ *       example:
+ *         id: 21
+ *         warehouse_id: 3
+ *         item_name: Television
+ *         description: This 50\", 4K LED TV provides a crystal-clear picture and vivid colors
+ *         category: Electronics
+ *         status: Out of Stock
+ *         quantity: 0
+ *         created_at: 2023-11-07T13:57:01.000Z
+ *         updated_at: 2023-11-07T13:57:01.000Z
+ * 
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: inventories
+ *   description: The inventories managing API
+ * /inventories:
+ *   get:
+ *     summary: Lists all the inventories
+ *     tags: [Inventories]
+ *     responses:
+ *       200:
+ *         description: The list of the inventories
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/inventories'
+ *   post:
+ *     summary: Create new inventory
+ *     tags: [Inventories]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/inventories'
+ *     responses:
+ *       200:
+ *         description: The created inventory.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/inventory'
+ *       500:
+ *         description: Some server error
+ * /inventories/{id}:
+ *   get:
+ *     summary: Get the inventory by id
+ *     tags: [Inventories]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The inventory id
+ *     responses:
+ *       200:
+ *         description: The inventory response by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/inventories'
+ *       404:
+ *         description: The inventory was not found
+ *   delete:
+ *     summary: Remove the inventory by id
+ *     tags: [Inventories]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The inventory id
+ *
+ *     responses:
+ *       200:
+ *         description: The inventory was deleted
+ *       404:
+ *         description: The inventory was not found
+ *   patch:
+ *    summary: Update the inventory by the id
+ *    tags: [Inventories]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The inventory id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/inventories'
+ *    responses:
+ *      200:
+ *        description: The inventory was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/inventories'
+ *      404:
+ *        description: The inventory was not found
+ *      500:
+ *        description: Some error happened
+ */
+
+
 import express from "express";
 import inventoryController from "../controllers/inventory-controller.js";
 
